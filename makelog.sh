21a31b1c2e155d3c20024fad7fc3798d917cd898
phan <ldh@qq.com>
2017-04-22 17:27:08 +0800

    更改判断文件大小的对象

--- makelog.sh
+++ makelog.sh
@@ -9,7 +9,7 @@ makelog(){
         date_str=$2
         loghtmlpath=loghtml/${date_str}.html
         goaccess $1 -a > ${loghtmlpath}
-        logsize=$(stat -c "%s" ${loghtmlpath})
+        logsize=$(stat -c "%s" $1)
         logsize_k=$(bc <<< "scale=2;${logsize}/1024")
         sql_str="insert into gef_infos(logdate,logsize,path) values(${date_str},${logsize_k},'${loghtmlpath}')"
         mysql -ushixin_logview -p<password>  shixin_logview -e "${sql_str}"
